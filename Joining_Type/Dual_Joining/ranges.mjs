9a2dd6fb714b5fe6f1d73d36f5f57ae00129d1ce
Mathias Bynens <mathias@qiwi.be>
2026-09-09 18:24:04 +0200

    Release v2.0.5

---
+++ Joining_Type/Dual_Joining/ranges.mjs
@@ -0,0 +1,3 @@
+import decodeRanges from '../../decode-ranges.mjs';
+
+export default decodeRanges('wYgAEAAAAEDMAGABgiBHPRglABIAAAABgnCBARCEDAIAAAAABeKCOBDAABCBFhJgghVEABAEADAAhJAAHw9cAXhXOghAA4I-Ugx');
